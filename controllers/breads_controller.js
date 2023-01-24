3e364f36c76cabe0c11fec90fd877524987114a6
8cott <scottrubin@gmail.com>
2023-01-23 20:46:37 -0500

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -33,6 +33,14 @@ breads.get('/new', (req, res) => {
   res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 // ---SHOW---
 // GET
 breads.get('/:arrayIndex', (req, res) => {
@@ -46,8 +54,20 @@ breads.get('/:arrayIndex', (req, res) => {
   }
 })
 
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+  console.log(req.body)
+  if(req.body.hasGluten === 'on'){
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
+})
+
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
+breads.delete('/:arrayIndex', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
 })
@@ -63,5 +83,7 @@ breads.delete('/:indexArray', (req, res) => {
 //   }
   
 // })
+
+
 
 module.exports = breads
