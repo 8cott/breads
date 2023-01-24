db28c096bc187bf17f067ad826974df43f7737a7
8cott <scottrubin@gmail.com>
2023-01-24 15:36:49 -0500

    added favicon

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // DEPENDENCIES
 const express = require('express')
 const methodOverride = require('method-override')
-
+const favicon = require('serve-favicon')
+const path = require('path')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -15,6 +16,8 @@ app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
 
 // ROUTES
 app.get('/', (req, res) => {
